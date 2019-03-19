498a9dc6fe930a8a056b1a6b9855dd46037ee4f2
Marvin Martens <marvin.martens@maastrichtuniversity.nl>
2019-03-19 17:24:43 +0100

    small adjustments

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM busybox:latest
 ENV APP_ROOT /code
 WORKDIR /data
 COPY aopwiki.ttl .
+COPY aopwiki.ttl.graph .
 COPY docker-entrypoint.sh ${APP_ROOT}/
 VOLUME /data
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
-echo "+> Copying /src to /dst..."
+echo "+> Copying data to target volume..."
 cp -r /data/* /aopwikidata
+ls
+ls
+
+echo "+> Post exit pause (5min)..."
+   sleep 300
 echo "+> Done."
